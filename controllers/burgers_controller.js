16777043cf6f0ef0b1826af858c478937f9a3cc9
taddes <taddes.korris@gmail.com>
2018-04-03 14:43:55 -0400

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,11 @@
+/*
+Inside your burger directory, create a folder named controllers.
+
+In controllers, create the burgers_controller.js file.
+
+Inside the burgers_controller.js file, import the following:
+
+Express
+burger.js
+Create the router for the app, and export the router at the end of your file.
+*/
