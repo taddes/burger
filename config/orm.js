d1259f5ff8ddf99d3909dfce6c9c53316a0849d2
Taddes Korris <taddes.korris@gmail.com>
2018-04-09 09:46:46 -0400

    commit of all files and directories

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,101 @@
-/*In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers.
+// These are the methods needed to retrieve and store data in  database.
 
-selectAll()
-insertOne()
-updateOne()
-*/
+
+var connection = require("../config/connection.js");
+
+	function printQuestionMarks(num){
+
+    var arr =[];
+    
+			for (var i =0; i < num; i++){
+
+        arr.ush('?');
+        
+      };
+      
+			return arr.toString();
+	};
+
+	function objToSql(ob){
+
+    var arr=[];
+    
+		for (var key in ob){
+			if ( ob.hasOwnProperty(key)){
+				arr.push(key + "=" + ob[key]);
+			}
+			
+    };
+    
+		return arr.toString();
+	};
+
+  // ORM
+
+var orm = {
+
+	all: function(tableInput, cb){
+
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      
+			connection.query(queryString, function(err, result) {
+
+        if (err) throw err;	
+        
+				cb(result);
+			});
+  },
+  
+	// vals the array that we want to save to cols
+	// cols are the columns we want to insert the values into
+	create: function(table, cols, vals, cb){
+			var queryString = "INSERT INTO " + table ;
+			queryString = queryString + " (";
+			queryString = queryString + cols.toString();
+			queryString = queryString + ") ";
+			queryString = queryString + "VALUES (";
+			queryString = queryString + printQuestionMarks(vals.length);
+			queryString = queryString + ") ";
+			console.log(queryString);
+
+			connection.query(queryString, vals, function(err, result){
+
+        if (err) throw err;
+        
+        cb(result);
+        
+			});
+	},
+
+	// objColVals are columns and values that need to be updated
+	update: function(table, objColVals, condition, cb){
+
+			var queryString = "UPDATE " + table;
+			queryString = queryString + " SET ";
+			queryString = queryString + objToSql(objColVals);
+			queryString = queryString + " WHERE " ;
+			queryString = queryString + condition;
+			console.log(queryString);
+
+			connection.query(queryString, function(err, result){
+				if (err) throw err;
+				cb(result);
+			});
+	},
+
+	delete: function(table, condition, cb){
+
+		var queryString= "DELETE FROM " + table;
+		queryString = queryString + " WHERE ";
+		queryString = queryString + condition;
+
+		connection.query(queryString, function(err, result){
+			if (err) throw err;
+      cb(result);
+      
+		});
+	}
+};
+
+module.exports = orm;
